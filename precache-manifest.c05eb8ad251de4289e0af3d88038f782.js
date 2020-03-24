4ceb7cebd71433a9dd698ea1bc89149e408cabe8
JadeMM <jade.moksness@gmail.com>
2020-03-24 14:55:23 -0700

    Updates

---
+++ precache-manifest.c05eb8ad251de4289e0af3d88038f782.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bfbe9a8781e71493f29bc4fb1ddf20c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2ae823e10c3ce80f9ccb",
+    "url": "/portfolio/static/css/main.94e7e712.chunk.css"
+  },
+  {
+    "revision": "88f57c11dca4288a0125",
+    "url": "/portfolio/static/js/2.9b1a1b16.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.9b1a1b16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ae823e10c3ce80f9ccb",
+    "url": "/portfolio/static/js/main.78053fc6.chunk.js"
+  },
+  {
+    "revision": "3cf4dcbd66b97c4ac79a",
+    "url": "/portfolio/static/js/runtime-main.743c8c5c.js"
+  },
+  {
+    "revision": "c62bcec1e7f6c7576a67012283ccc1da",
+    "url": "/portfolio/static/media/background.c62bcec1.jpg"
+  },
+  {
+    "revision": "d2d90f26dc047352084b7e2239e3240c",
+    "url": "/portfolio/static/media/profile.d2d90f26.jpg"
+  }
+]);
